12bb37ef1dfd0c30d1a735f434e9a5cf7dd28500
HeyitsFelix <86806462+HeyitsFelix@users.noreply.github.com>
2024-12-06 21:23:49 -0600

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -6,6 +6,6 @@ self.__uv$config = {
     handler: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.10/dist/uv.handler.js',
     client: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.10/dist/uv.client.js',
     bundle: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.10/dist/uv.bundle.js',
-    config: '/uv.config.js',
+    config: 'https://cdn.jsdelivr.net/gh/HeyitsFelix/Ultraviolet-filefix@32bdbb7d0d4190bf78943713a55a03d6e70b7995/uv.config.js',
     sw: 'https://cdn.jsdelivr.net/gh/HeyitsFelix/Ultraviolet-filefix@a1aad4065a320ca862f1d55d543ee05f9b82e19f/uv.sw.js',
 };

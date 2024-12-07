daa795348af95f413a6c40e92b317ec29e842c9e
HeyitsFelix <86806462+HeyitsFelix@users.noreply.github.com>
2024-12-06 21:26:50 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
  */
 importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.10/dist/uv.bundle.js');
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@3.2.10/dist/uv.config.js');
+importScripts('https://cdn.jsdelivr.net/gh/HeyitsFelix/Ultraviolet-filefix@8d807ef6be812b51423cf91bd2a94d6d12196868/uv.config.js');
 importScripts(__uv$config.sw || 'https://cdn.jsdelivr.net/gh/HeyitsFelix/Ultraviolet-filefix@91234072ade29accd59192e9df5f775334348a58/uv.sw.js');
 
 const uv = new UVServiceWorker();
